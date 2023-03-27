d57a140787691f0cceb49c7ac6cabcb090f3da86
ungyum <simhonfumn58@gmail.com>
2023-03-27 12:12:37 +0900

    프런트 단 js 만들기

--- app/app.js
+++ app/app.js
@@ -10,8 +10,10 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`)); // 이렇게 해 놔야 렌더가 js를 읽을 수 있음. 그리고 디렉토리를 public으로 설정해줘서 script src에 바로 js/home 처럼 쓰면 됨
+// 브라우저의 요청에 따라 꺼내줄 수 있는 파일이기 때문에 public안에 들어가는듯
 
-// route
+// 라우팅
 app.use("/", home); // use는 미들웨어를 등록해주는 메소드
 
 module.exports = app;
