cefd50d29950eecc3514724752a73664da0726a6
ungyum <simhonfumn58@gmail.com>
2023-03-27 11:54:26 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+// route
+app.use("/", home); // use는 미들웨어를 등록해주는 메소드
+
+module.exports = app;
