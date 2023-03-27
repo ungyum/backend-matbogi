634fa3b1b04852b9d98dc056552479fee7c8ab1d
ungyum <simhonfumn58@gmail.com>
2023-03-27 12:39:04 +0900

    로그인창 js

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
 "use strict";
 
-// console.log("hello");
+const idInput = document.getElementById("id"),
+  pwInput = document.getElementById("pw"),
+  loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", login);
+function login() {
+  const req = {
+    id: idInput.value,
+    pw: pwInput.value,
+  };
+  console.log(req);
+}
